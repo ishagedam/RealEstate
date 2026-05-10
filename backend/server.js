db730b227710b309c0ea797429b0639714cf0c9f
isha <ishagedam2018@gmail.com>
2026-05-10 17:52:58 +0530

    updated server

--- backend/server.js
+++ backend/server.js
@@ -112,11 +112,18 @@ const startServer = async () => {
     await sequelize.sync();
     console.log("User table synced ✅");
 
-    const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT} 🚀`);
-    });
+    //  i am changed this for render
+// const PORT = process.env.PORT || 5000;
+//     app.listen(PORT, () => {
+//       console.log(`Server running on http://localhost:${PORT} 🚀`);
+//     });
 
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
   } catch (err) {
     console.error("DB Sync Error:", err);
   }
