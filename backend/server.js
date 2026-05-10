1362d98a7ad093ee0f4ba732d40b7644c6335df5
isha <ishagedam2018@gmail.com>
2026-05-10 15:58:30 +0530

    updated backend code

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,17 @@ import Enquiry from "./models/enquiryModel.js";
 
 // ================= CONFIG =================
 dotenv.config();
+
+
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+
+
+
 const app = express();
 
 // ================= MIDDLEWARE (SAFE & SIMPLE) =================
