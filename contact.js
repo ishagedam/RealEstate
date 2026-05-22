f01f46dbb676f10efed3b707a89811193de782fb
isha <ishagedam2018@gmail.com>
2026-05-22 07:33:20 +0530

    Added reusable footer component

--- contact.js
+++ contact.js
@@ -168,8 +168,3 @@ gsap.from(".forthsection", {
   const splitNav = new SplitType(".navbar h6", { types: "chars" });
   gsap.from(splitNav.chars, { y: -20, opacity: 0, duration: 0.5, stagger: 0.1 });
 
-fetch("footer/footer.html")
-.then(response => response.text())
-.then(data => {
-  document.getElementById("footer").innerHTML = data;
-});
